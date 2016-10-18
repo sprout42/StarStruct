46a9c7f8fbb896763f0dbd090b7b935fa33da145
Jace Browning <jacebrowning@gmail.com>
2016-10-17 22:07:34 -0400

    Update project from the template

--- setup.py
+++ setup.py
@@ -1,22 +1,50 @@
 #!/usr/bin/env python
 
-"""Setup script for StarStruct."""
+"""Setup script for the package."""
 
 import os
+import sys
+
 import setuptools
 
-from starstruct import __project__, __version__
 
-if os.path.exists('README.rst'):
-    README = open('README.rst').read()
-else:
-    README = ""  # a placeholder, readme is generated on release
-CHANGES = open('CHANGES.md').read()
+PACKAGE_NAME = 'starstruct'
+MINIMUM_PYTHON_VERSION = 3, 5
 
+
+def check_python_version():
+    """Exit when the Python version is too low."""
+    if sys.version_info < MINIMUM_PYTHON_VERSION:
+        sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
+
+
+def read_package_variable(key):
+    """Read the value of a variable from the package without importing."""
+    module_path = os.path.join(PACKAGE_NAME, '__init__.py')
+    with open(module_path) as module:
+        for line in module:
+            parts = line.strip().split(' ')
+            if parts and parts[0] == key:
+                return parts[-1].strip("'")
+    assert 0, "'{0}' not found in '{1}'".format(key, module_path)
+
+
+def read_descriptions():
+    """Build a description for the project from documentation files."""
+    try:
+        readme = open("README.rst").read()
+        changelog = open("CHANGELOG.rst").read()
+    except IOError:
+        return "<placeholder>"
+    else:
+        return readme + '\n' + changelog
+
+
+check_python_version()
 
 setuptools.setup(
-    name=__project__,
-    version=__version__,
+    name=read_package_variable('__project__'),
+    version=read_package_variable('__version__'),
 
     description="StarStruct allows for easy binary stream pack/unpack",
     url='https://github.com/sprout42/StarStruct',
@@ -27,7 +55,7 @@ setuptools.setup(
 
     entry_points={'console_scripts': []},
 
-    long_description=(README + '\n' + CHANGES),
+    long_description=read_descriptions(),
     license='MIT',
     classifiers=[
         'Development Status :: 1 - Planning',
@@ -36,5 +64,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
     ],
 
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=open("requirements.txt").readlines(),
 )
