3229561399c88760ae19c08c81b77a842930ae51
TJ DeVries <tim.devries@dornerworks.com>
2016-12-19 14:47:45 -0500

    Add elementnone

---
+++ starstruct/elementnone.py
@@ -0,0 +1,62 @@
+"""
+Element None
+
+A non-packable, non-unpackable item that can help facilitate with additional information about an object,
+pass extra references to a variable and potentially more
+
+.. code-block:: python
+
+    ExampleMessage = Message('VarTest', [('x', 'B'), ('y', 'B')])
+
+    CRCedMessage = Message('CRCedMessage', [
+       ('data', ExampleMessage),                  # A data field that has the example message in it
+       ('extra_param', None),                     # The None Element
+       ('crc', 'I', my_crc32, ['data', 'extra_param']),          # Crc the data, and give an error if we have something unexpected
+   ])
+
+"""
+
+from typing import Optional
+
+from starstruct.element import register, Element
+from starstruct.modes import Mode
+
+@register
+class ElementNone(Element):
+    """
+    Initialize a StarStruct element object.
+
+    :param field: The fields passed into the constructor of the element
+    :param mode: The mode in which to pack the bytes
+    :param alignment: Number of bytes to align to
+    """
+    def __init__(self, field: list, mode: Optional[Mode]=Mode.Native, alignment: Optional[int]=1):
+        self.name = field[0]
+
+        self.update(mode=mode, alignment=alignment)
+
+    @staticmethod
+    def valid(field: tuple) -> bool:
+        return len(field) == 2 \
+            and isinstance(field[0], str) \
+            and field[1] is None
+
+    def validate(self, msg):
+        return True
+
+    def update(self, mode=None, alignment=None):
+        if mode:
+            self._mode = mode
+
+        if alignment:
+            self._alignment = alignment
+        return
+
+    def pack(self, msg):
+        return b''
+
+    def unpack(self, msg, buf):
+        return (msg, buf)
+
+    def make(self, msg):
+        return msg[self.name]
---
+++ starstruct/tests/test_elementnone.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""Tests for the starstruct class"""
+
+import unittest
+from hashlib import md5
+
+from starstruct.message import Message
+
+class TestStarStructNone(unittest.TestCase):
+    """StarStruct module tests"""
+
+    VarTest = Message('VarTest', [
+        ('x', 'B'),
+        ('y', 'B'),
+    ])
+
+    def test_single_element_2(self):
+        def pseudo_salted_md5(salt, original):
+            temp_md5 = md5(original)
+            return md5(salt + temp_md5.digest()).digest()
+
+        TestStruct = Message('TestStruct', [
+            ('length_in_objects', 'H', 'vardata'),
+            ('vardata', self.VarTest, 'length_in_objects'),
+        ])
+
+        CRCedMessage = Message('CRCedMessage', [
+            ('data', TestStruct),
+            ('salted', None),
+            ('function_data', '16B', pseudo_salted_md5, ['salted', b'data']),
+        ])
+
+        test_data = {
+            'data': {
+                'length_in_objects': 2,
+                'vardata': [
+                    {'x': 1, 'y': 2},
+                    {'x': 3, 'y': 4},
+                ],
+            },
+            'salted': b'random_salter',
+        }
+
+        made = CRCedMessage.make(test_data)
+        # assert len(made) == 5
+        assert len(made.data.vardata) == 2
+        assert made.data.vardata[0].x == 1
+        assert made.data.vardata[0].y == 2
+
+        no_data = made.pack()
+        regular = CRCedMessage.pack(**test_data)
+        assert regular == no_data
